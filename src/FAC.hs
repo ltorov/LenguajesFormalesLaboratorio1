c159b923ddf8ce332bc00756b90670a0c75066f4
Luisa Toro Villegas <60147146+ltorov@users.noreply.github.com>
2021-03-17 17:23:56 -0500

    Add files via upload

--- src/FAC.hs
+++ src/FAC.hs
@@ -34,7 +34,7 @@ groupBy (x:xs) c r
     | x == c        = r : groupBy xs c ""
     | otherwise     = groupBy xs c (r ++ [x])
 
--- Auxiliary functions:
+-- AUXILIARY FUNCTIONS:
 
 --Extract the last element of a list, which must be finite and non-empty. The function generates an error if the list is empty.
 myLast :: [a] -> a
@@ -52,11 +52,43 @@ stringToList :: String -> [String]
 stringToList [] = []
 stringToList a = groupBy a ',' []
 
+--NON AUXILIARY METHODS
+---Prove that it is a finite automata
+
+--Check if there are states
+emptyStates :: FA Int -> Bool
+emptyStates a = statesFA a /= Set.empty
+
+{- --Check if start state is empty
+emptyStartState :: FA Int -> Bool
+emptyStartState a = startStateFA a == "" -}
+
+--Check if start state belongs to the states
+startBelongs :: FA Int -> Bool
+startBelongs a = Set.notMember (startStateFA a)  (statesFA a)
+
+--Check if final states are a subset of the states
+finalBelongs :: FA Int -> Bool
+finalBelongs a = Set.isSubsetOf (finalStatesFA a) (statesFA a)
+
+--Check if the states used for the transtition belong to the set of states
+--Todavia no lo quiero hacer ups perdon
+
+--Put together all the booleans and determine if it is a finite automata
+isAutomata :: FA Int -> Bool
+isAutomata a = emptyStates a && startBelongs a && finalBelongs a
+
+---Prove if it is and e-NFA (epsilon)
+isENFA :: FA Int -> Bool
+isENFA a = Set.isSubsetOf (movesFA) ()
+
+
 --Reads the input into a FA
 main :: IO ()
 main = do 
-    input <- readFile "FA1.txt"
+    input <- readFile "FA4.txt"
     let automaton = read input :: FA Int
+    print(finalBelongs automaton)
 
     
      
