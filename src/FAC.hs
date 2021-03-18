912694ac5ddc5c0cd7484f856a6cb5de6c3bb0d5
Luisa Toro Villegas <60147146+ltorov@users.noreply.github.com>
2021-03-18 16:25:43 -0500

    Add files via upload

--- src/FAC.hs
+++ src/FAC.hs
@@ -4,23 +4,7 @@ import Data.List ( nub )
 import Data.Set ( Set )
 import Text.Read (readMaybe)
 import qualified Data.Set as Set
-
--- | Moves (or transitions) data type.
-data Move a = Move a Char a | Emove a a
-           deriving (Eq, Ord, Show, Read)
-
--- | Finite automata data type.
-data FA a  = FA { statesFA      :: Set a
-                , movesFA       :: Set (Move a)
-                , startStateFA  :: a
-                , finalStatesFA :: Set a
-                }
-          deriving (Show, Read, Eq)
-
--- | 'alphabetFA' returns the alphabet of the finite automata, by
---   finding a list of the characters mentioned in the moves.
-alphabetFA :: FA a -> [Char]
-alphabetFA (FA _ moves _ _) = nub [ c | Move _ c _ <- Set.toList moves ]
+import FATypes
 
 --Auxiliar method to separate a string into an array
 --Taken from: https://codereview.stackexchange.com/questions/6992/approach-to-string-split-by-character-in-haskell
@@ -38,7 +22,7 @@ groupBy (x:xs) c r
 
 --Extract the last element of a list, which must be finite and non-empty. The function generates an error if the list is empty.
 myLast :: [a] -> a
-myLast [] = error "Empty list."
+myLast  [] = error "Empty list."
 myLast [x] = x
 myLast (x : xs) = myLast xs
 
@@ -57,7 +41,7 @@ stringToList a = groupBy a ',' []
 
 --Check if there are states
 emptyStates :: FA Int -> Bool
-emptyStates a = statesFA a /= Set.empty
+emptyStates a = faStates a /= Set.empty
 
 {- --Check if start state is empty
 emptyStartState :: FA Int -> Bool
@@ -65,11 +49,11 @@ emptyStartState a = startStateFA a == "" -}
 
 --Check if start state belongs to the states
 startBelongs :: FA Int -> Bool
-startBelongs a = Set.notMember (startStateFA a)  (statesFA a)
+startBelongs a = Set.notMember (faStartState a)  (faStates a)
 
 --Check if final states are a subset of the states
 finalBelongs :: FA Int -> Bool
-finalBelongs a = Set.isSubsetOf (finalStatesFA a) (statesFA a)
+finalBelongs a = Set.isSubsetOf (faFinalStates a) (faStates a)
 
 --Check if the states used for the transtition belong to the set of states
 --Todavia no lo quiero hacer ups perdon
@@ -78,9 +62,14 @@ finalBelongs a = Set.isSubsetOf (finalStatesFA a) (statesFA a)
 isAutomata :: FA Int -> Bool
 isAutomata a = emptyStates a && startBelongs a && finalBelongs a
 
+
+isEmove :: Move a -> Bool
+isEmove (Move s1 c s2) = False
+isEmove (Emove s1 s2)  = True
+
 ---Prove if it is and e-NFA (epsilon)
 isENFA :: FA Int -> Bool
-isENFA a = 
+isENFA a = False
 
 
 --Reads the input into a FA
@@ -89,15 +78,3 @@ main = do
     input <- readFile "FA4.txt"
     let automaton = read input :: FA Int
     print(finalBelongs automaton)
-
-    
-     
-    
-   
-
-
-   
-  
-
-
-
