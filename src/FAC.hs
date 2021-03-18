72e49c3119025c1ceebed2d79c06cb7b0f5d44de
Luisa Toro Villegas <60147146+ltorov@users.noreply.github.com>
2021-03-18 18:14:51 -0500

    Add files via upload

--- src/FAC.hs
+++ src/FAC.hs
@@ -6,81 +6,49 @@ import Text.Read (readMaybe)
 import qualified Data.Set as Set
 import FATypes
 
---Auxiliar method to separate a string into an array
---Taken from: https://codereview.stackexchange.com/questions/6992/approach-to-string-split-by-character-in-haskell
-groupBy :: String -> Char -> String -> [String]
-groupBy ""     _ "" = []
-groupBy ""     _ r  = [r]
-groupBy (x:xs) c ""
-    | x == c        = groupBy xs c ""
-    | otherwise     = groupBy xs c [x]
-groupBy (x:xs) c r
-    | x == c        = r : groupBy xs c ""
-    | otherwise     = groupBy xs c (r ++ [x])
-
--- AUXILIARY FUNCTIONS:
-
---Extract the last element of a list, which must be finite and non-empty. The function generates an error if the list is empty.
-myLast :: [a] -> a
-myLast  [] = error "Empty list."
-myLast [x] = x
-myLast (x : xs) = myLast xs
-
---Removes the last element from a list.
-pop :: [a] -> [a]
-pop [] = error "Empty list."
-pop xs = init xs
-
---String to list
-stringToList :: String -> [String]
-stringToList [] = []
-stringToList a = groupBy a ',' []
-
---NON AUXILIARY METHODS
----Prove that it is a finite automata
-
---Check if there are states
+---FINITE AUTOMATA
+--Check if the automata has any states
 emptyStates :: FA Int -> Bool
 emptyStates a = faStates a /= Set.empty
 
-{- --Check if start state is empty
-emptyStartState :: FA Int -> Bool
-emptyStartState a = startStateFA a == "" -}
-
---Check if start state belongs to the states
+--Check if the automata's start state belongs to its states
 startBelongs :: FA Int -> Bool
 startBelongs a = Set.member (faStartState a) (faStates a)
 
---Check if final states are a subset of the states
+--Check if the automata's final states are a subset of its states
 finalBelongs :: FA Int -> Bool
 finalBelongs a = Set.isSubsetOf (faFinalStates a) (faStates a)
 
-{- --Check if the states used for the transtition belong to the set of states
---Todavia no lo quiero hacer ups perdon
+{- --Check if the states used for the transition belong to the set of states
 moveBelongs :: FA a -> Move a -> Bool
 moveBelongs a (Move s1 c s2) = Set.notMember s1 (faStates a)
 moveBelongs a (Emove s1 s2)  = Set.notMember s1 (faStates a) -}
 
---Put together all the booleans and determine if it is a finite automata
+--Put together all the functions above and determine if it is a finite automata
 isAutomata :: FA Int -> Bool
 isAutomata a = emptyStates a && startBelongs a && finalBelongs a
 
+--FINITE AUTOMATA TYPE
+---e-NFA
 --Check whether a move is of the type emove by pattern matching
 isEmove :: Move a -> Bool
 isEmove (Move s1 c s2) = False
 isEmove (Emove s1 s2)  = True
 
----Prove if it is an e-NFA (epsilon)
+-- if it is an e-NFA (epsilon)
 isENFA :: [Move a] -> Bool
 isENFA []     = False
 isENFA [x]    = isEmove x
 isENFA (x:xs) = isEmove x || isENFA xs
+
+---NFA
 {- 
 --Prove if it is an NFA 
 isNFA :: FA Int ->Bool
 isNFA a = (isENFA (Set.toList(faMoves a)) == False) && (isDFA a == False)
 
---Prove if it is an DFA
+---DFA
+--
 --Compares if when two moves begin in the same state they have the same char
 auxIsDFA :: Move a -> Move a -> Bool
 auxIsDFA (Move s1 c1 s2) (Move s3 c2 s4) = ( s1 == s3 && c1 == c2 ) 
@@ -92,10 +60,11 @@ compareMoves [x]       = True
 compareMoves [x,y]     = auxIsDFA x y
 compareMoves (x:y:xs)  = (auxIsDFA x y) && compareMoves (x:xs) && compareMoves (y:xs)
 
---
+--Check if it is a DFA
 isDFA :: FA Int -> Bool
 isDFA a = compareMoves(Set.toList(faMoves a))
  -}
+
 --Reads the input into a FA and takes the name of the file to be read as what the user inputs
 main :: IO ()
 main = do 
