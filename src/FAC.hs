942511056a361d9aa5d6ae6242bd99ab67ea6e18
Luisa Toro Villegas <60147146+ltorov@users.noreply.github.com>
2021-03-19 15:37:00 -0500

    Add files via upload

--- src/FAC.hs
+++ src/FAC.hs
@@ -1,6 +1,3 @@
---Joint lab with Gregorio Pérez Bernal and Luisa Toro Villegas
--- GHCi, version 8.10.4 and Stack version: 2.5.1
-
 import System.IO --Para leer y escribir archivos
 import Data.Typeable
 import Data.List ( nub )
@@ -14,22 +11,13 @@ import FATypes
 emptyStates :: FA Int -> Bool
 emptyStates a = faStates a /= Set.empty
 
-reasonEmptyStates :: Bool -> String -> String
-reasonEmptyStates False str = "The automaton in "++ str ++ " is invalid because it has not states"
-
 --Check if the automata's start state belongs to its states
 startBelongs :: FA Int -> Bool
 startBelongs a = Set.member (faStartState a) (faStates a)
 
-reasonStartBelongs :: Bool -> String -> String
-reasonStartBelongs False str = "The automaton in "++ str ++ " is invalid because the start state is not a valid state"
-
 --Check if the automata's final states are a subset of its states
 finalBelongs :: FA Int -> Bool
 finalBelongs a = Set.isSubsetOf (faFinalStates a) (faStates a)
-
-reasonFinalBelongs :: Bool -> String -> String
-reasonFinalBelongs False str = "The automaton in "++ str ++ " is invalid because the final states are not a subset of the valid states"
 
 --Check if the states used for the transition belong to the set of states
 moveBelongsAux :: Ord a => FA a -> Move a -> Bool
@@ -44,21 +32,18 @@ moveBelongsList a (x:xs) = (moveBelongsList a [x]) && (moveBelongsList a xs)
 moveBelongs :: Ord a => FA a -> Bool
 moveBelongs a = moveBelongsList a (Set.toList(faMoves a))
 
-reasonMoveBelongs :: Bool -> String -> String
-reasonMoveBelongs False str = "The automaton in "++ str ++ " is invalid because some states used in the transition are not valid states"
-
 --Put together all the functions above and determine if it is a finite automata
 
 isAutomata :: FA Int -> Bool
-isAutomata a = emptyStates a && startBelongs a && finalBelongs a && (moveBelongs a (Set.toList(faMoves a)))
+isAutomata a = emptyStates a && startBelongs a && finalBelongs a && moveBelongs a
 
-invalidAutomata :: FA Int -> String -> Bool
-invalidAutomata a str
-    | (isAutomata a == False), (emptyStates a == True)  = reasonEmptyStates (emptyStates a == True) str
-    | (isAutomata a == False), (startBelongs a == True) = reasonStartBelongs (startBelongs a == True) str
-    | (isAutomata a == False), (finalBelongs a == True) = reasonFinalBelongs (finalBelongs a == True) str
-    | (isAutomata a == False), (moveBelongs a == True)  = reasonMoveBelongs (moveBelongs a == True) str
-
+printAutomata :: FA Int -> String -> String
+printAutomata a str
+    | emptyStates a == False  = "The automaton in "++ str ++ " is invalid because it has not states"
+    | startBelongs a == False =  "The automaton in "++ str ++ " is invalid because the start state is not a valid state"
+    | finalBelongs a == False = "The automaton in "++ str ++ " is invalid because the final states are not a subset of the valid states"
+    | moveBelongs a == False  =  "The automaton in "++ str ++ " is invalid because some states used in the transition are not valid states"
+    | isAutomata a = classifyAutomata a str
 --FINITE AUTOMATA TYPE
 ---e-NFA
 --Check whether a move is of the type emove by pattern matching
@@ -115,13 +100,10 @@ classifyAutomata a str
     | isDFA a == True  = printDFA (isDFA a) str
     | isENFA a == True = printENFA (isENFA a) str
 
-
 --Reads the input into a FA and takes the name of the file to be read as what the user inputs
 main :: IO ()
 main = do 
     line <- getLine
     input <- readFile line
     let automata = read input :: FA Int
-    print(classifyAutomata automata line)
-    {- let movesList = Set.toList(faMoves automata) --Para correr el isENFA
-    print(isENFA movesList) -}
+    print(printAutomata automata line)
