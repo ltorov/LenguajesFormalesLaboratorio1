119b0bd54682eb45a2148319fdef059cc3ad65cb
Luisa Toro Villegas <60147146+ltorov@users.noreply.github.com>
2021-03-19 10:55:17 -0500

    Add files via upload

--- src/FAC.hs
+++ src/FAC.hs
@@ -6,7 +6,7 @@ import Text.Read (readMaybe)
 import qualified Data.Set as Set
 import FATypes
 
----FINITE AUTOMATA
+---FINITE AUTOMATA1 ººººº
 --Check if the automata has any states
 emptyStates :: FA Int -> Bool
 emptyStates a = faStates a /= Set.empty
@@ -42,7 +42,6 @@ isENFA [x]    = isEmove x
 isENFA (x:xs) = isEmove x || isENFA xs
 
 ---NFA
-{- 
 --Prove if it is an NFA 
 isNFA :: FA Int ->Bool
 isNFA a = (isENFA (Set.toList(faMoves a)) == False) && (isDFA a == False)
@@ -50,27 +49,29 @@ isNFA a = (isENFA (Set.toList(faMoves a)) == False) && (isDFA a == False)
 ---DFA
 --
 --Compares if when two moves begin in the same state they have the same char
-auxIsDFA :: Move a -> Move a -> Bool
-auxIsDFA (Move s1 c1 s2) (Move s3 c2 s4) = ( s1 == s3 && c1 == c2 ) 
+auxIsDFA :: Eq a => Move a -> Move a -> Bool
+auxIsDFA (Move s1 c1 s2) (Move s3 c2 s4) = (( s1 == s3) && (c1 == c2)) == False
+auxIsDFA (Emove s1  s2) (Move s3 c2 s4)  = False
+auxIsDFA (Move s1 c1 s2) (Emove s3 s4)   = False
+auxIsDFA (Emove s1 s2) (Emove s3 s4)     = False
 
 --Takes list and compares every pair of two moves
-compareMoves :: [Move a] ->  Bool
+compareMoves :: Eq a => [Move a] ->  Bool
 compareMoves []        = True --si no hay movimientos, es DFA?
 compareMoves [x]       = True 
 compareMoves [x,y]     = auxIsDFA x y
-compareMoves (x:y:xs)  = (auxIsDFA x y) && compareMoves (x:xs) && compareMoves (y:xs)
+compareMoves (x:y:xs)  = (auxIsDFA x y) || compareMoves (x:xs) || compareMoves (y:xs)
 
 --Check if it is a DFA
 isDFA :: FA Int -> Bool
 isDFA a = compareMoves(Set.toList(faMoves a))
- -}
 
 --Reads the input into a FA and takes the name of the file to be read as what the user inputs
 main :: IO ()
 main = do 
     line <- getLine
     input <- readFile line
-    let automaton = read input :: FA Int
-    print(isAutomata automaton)
+    let automata = read input :: FA Int
+    print(isDFA automata)
     {- let movesList = Set.toList(faMoves automaton) --Para correr el isENFA
     print(isENFA movesList) -}
